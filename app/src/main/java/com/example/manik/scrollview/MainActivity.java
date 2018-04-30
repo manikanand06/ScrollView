f247acd0c542e5e70a831512bf72acf7018d9460
Manik Anand <infomanik.anand@gmail.com>
2018-04-30 22:31:20 +0530

    Init

---
+++ app/src/main/java/com/example/manik/scrollview/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.manik.scrollview;
+
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+//TextView txtScroll = (TextView) findViewById(R.id.txtscroll);
+//txtScroll.setMovementMethod(new ScrollingMovementMethod());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
